a3dd85f74a3c652db0d03ffe7a2a436a490bf858
Joy Latten <jlatten@cisco.com>
2022-12-13 17:03:19 -0600

    Add support function to generate sudi cert and private key for a VM.
    Signed-off-by: Joy Latten <jlatten@cisco.com>

--- utils.go
+++ utils.go
@@ -3,6 +3,7 @@ package main
 import (
 	"os"
 	"path/filepath"
+	"github.com/go-git/go-git/v5"
 )
 
 func PathExists(d string) bool {
@@ -20,4 +21,43 @@ func getTrustPath() (string, error) {
 	}
 	path := filepath.Join(configDir, "machine", "trust")
 	return path, os.MkdirAll(path, 0755)
+}
+
+func getSudiDir() (string, error) {
+	dataDir, err := UserDataDir()
+	if err != nil {
+		return "", err
+	}
+	sudiPath := filepath.Join(dataDir, "machine", "trust")
+	return sudiPath, os.MkdirAll(sudiPath, 0755)
+}
+
+func UserDataDir() (string, error) {
+	p, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+	return filepath.Join(p, ".local", "share"), nil
+}
+
+func ConfPath(cluster string) string {
+	configDir, err := os.UserConfigDir()
+	if err != nil {
+		return ""
+	}
+	return filepath.Join(configDir, "machine", cluster, "machine.yaml")
+}
+
+// Get the keys repo to use keys.
+// Callers are responsible for removing the repo when done.
+func getKeysrepo() (string, error) {
+	dir, err := os.MkdirTemp("", "keys")
+	if err != nil {
+		return "", err
+	}
+	_, err = git.PlainClone(dir, false, &git.CloneOptions{URL: "https://github.com/project-machine/keys.git",})
+	if err != nil {
+		return "", err
+	}
+	return dir, nil
 }
