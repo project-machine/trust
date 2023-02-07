904036e9aeebbea4085952880353715d9c63bd53
Joy Latten <jlatten@cisco.com>
2023-02-07 11:48:59 -0600

    Replace openssl commands with golang api.
    This commit also has some corrections from running
    golint and go fmt on cert.go
    
    Signed-off-by: Joy Latten <jlatten@cisco.com>

--- pkg/trust/cert.go
+++ pkg/trust/cert.go
@@ -1,17 +1,20 @@
 package trust
 
 import (
+	"crypto"
+	"crypto/rsa"
+	"crypto/sha256"
 	"crypto/x509"
 	"encoding/pem"
 	"fmt"
 	"os"
-	"path/filepath"
 )
 
-// Check that the product cert was signed by the global puzzleos cert
-// This version can be used by outside callers, like atomix extract-soci
-// Note that this version does not verify product pid.
-func VerifyCert(cert []byte, caPath string) error {
+// VerifyCert checks that the product cert was signed by the
+// global puzzleos cert. This version can be used by outside
+// callers, like atomix extract-soci. Note that this version
+// does not verify product pid.
+func VerifyCert(parsedCert *x509.Certificate, caPath string) error {
 	paths := []string{
 		"/factory/secure/manifestCA.pem",
 		"/factory/secure/layerCA.pem",
@@ -39,15 +42,6 @@ func VerifyCert(cert []byte, caPath string) error {
 		return fmt.Errorf("Failed adding cert from OCI signing CA")
 	}
 
-	block, _ := pem.Decode(cert)
-	if block == nil {
-		return fmt.Errorf("Failed to parse manifest-signing certificate PEM: %w", err)
-	}
-	parsedCert, err := x509.ParseCertificate(block.Bytes)
-	if err != nil {
-		return fmt.Errorf("Failed reading certificate from manifest: %w", err)
-	}
-
 	opts := x509.VerifyOptions{
 		Roots:     pool,
 		KeyUsages: []x509.ExtKeyUsage{x509.ExtKeyUsageAny},
@@ -59,55 +53,81 @@ func VerifyCert(cert []byte, caPath string) error {
 	return nil
 }
 
-// VerifyManifest checks that @contents is signed by 
+// VerifyManifest checks that @contents is signed by
 func VerifyManifest(contents []byte, sigPath, certPath, caPath string) error {
-	xtract := []string{"openssl", "x509", "-in", certPath, "-pubkey", "-noout"}
-	cert, err := os.ReadFile(certPath)
+	// Get the cert and extract the public key
+	var parsedCert *x509.Certificate
+	certPEM, err := os.ReadFile(certPath)
 	if err != nil {
 		return fmt.Errorf("Failed reading manifest cert (%q): %w", certPath, err)
 	}
-	pubout, _, err := RunWithStdall(string(cert), xtract...)
-
-	tmpd, err := os.MkdirTemp("", "pubkey")
-	if err != nil {
-		return fmt.Errorf("Failed creating a tempdir: %w", err)
+	block, _ := pem.Decode([]byte(certPEM))
+	if block == nil {
+		return fmt.Errorf("Failed to decode manifest cert (%q)", certPath)
 	}
-	defer os.RemoveAll(tmpd)
-	keyPath := filepath.Join(tmpd, "pub.key")
-	err = os.WriteFile(keyPath, []byte(pubout), 0600)
+	parsedCert, err = x509.ParseCertificate(block.Bytes)
 	if err != nil {
-		return fmt.Errorf("Failed writing out public key: %w", err)
+		return fmt.Errorf("Failed parsing manifest cert (%q): %w", certPath, err)
 	}
+	rsaPublicKey := parsedCert.PublicKey.(*rsa.PublicKey)
 
-	err = VerifyCert(cert, caPath)
+	// Verify the chain of trust
+	err = VerifyCert(parsedCert, caPath)
 	if err != nil {
 		return fmt.Errorf("Manifest certificate does not match the CA: %w", err)
 	}
 
-	cmd := []string{"openssl", "dgst", "-sha256", "-verify", keyPath,
-		"-signature", sigPath}
-	// Pass the manifest text (whose signature we are verifying) in over
-	// stdin to avoid a TOCTTOU between manifest reads.
-	stdout, stderr, err := RunWithStdall(string(contents), cmd...)
+	// Get the signature
+	signature, err := os.ReadFile(sigPath)
 	if err != nil {
-		errmsg := "Failed verifying manifest signature:\nStdout: %v\nStderr: %v\nError: %w"
-		return fmt.Errorf(errmsg, err, stdout, stderr)
+		return fmt.Errorf("Failed reading signature (%q): %w", sigPath, err)
+	}
+
+	// Verify signature
+	msghash := sha256.Sum256(contents)
+	err = rsa.VerifyPKCS1v15(rsaPublicKey, crypto.SHA256, msghash[:], signature)
+	if err != nil {
+		return fmt.Errorf("Failed verifying manifest signature: %w", err)
 	}
 
 	return nil
 }
 
-// Sign: sign a file
+// Sign signs a file
 // Sign the contents of @sourcePath using the key at @keyPath,
 // storing the result in the file called @signedpath
 func Sign(sourcePath, signedPath, keyPath string) error {
-	args := []string{"openssl", "dgst", "-sha256",
-		"-sign", keyPath,
-		"-out", signedPath,
-		sourcePath}
-	output, err := Run(args...)
+	// Get the key to use for signing
+	privKey, err := os.ReadFile(keyPath)
 	if err != nil {
-		return fmt.Errorf("Signing error: %w\nOutput: %s\n", err, output)
+		return fmt.Errorf("Failed reading (%q): %w", keyPath, err)
+	}
+	pemBlock, _ := pem.Decode(privKey)
+	if pemBlock == nil {
+		return fmt.Errorf("Failed to decode key: (%q)", keyPath)
+	}
+	pkcsKey, err := x509.ParsePKCS8PrivateKey(pemBlock.Bytes)
+	if err != nil {
+		return fmt.Errorf("Failed parsing key, (%q): %w", keyPath, err)
+	}
+
+	// Hash the data @sourcePath
+	msg, err := os.ReadFile(sourcePath)
+	if err != nil {
+		return fmt.Errorf("Failed reading (%q): %w", sourcePath, err)
+	}
+	msghash := sha256.Sum256(msg)
+
+	// Sign the hash
+	signature, err := rsa.SignPKCS1v15(nil, pkcsKey.(*rsa.PrivateKey), crypto.SHA256, msghash[:])
+	if err != nil {
+		return fmt.Errorf("Failed signing hash of (%q): %w", sourcePath, err)
+	}
+
+	// Write the signature to signedPath
+	err = os.WriteFile(signedPath, signature, 0640)
+	if err != nil {
+		return fmt.Errorf("Failed writing signature to (%q): %w", signedPath, err)
 	}
 
 	return nil
