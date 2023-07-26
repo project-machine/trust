008ebcb5d61f8853935fa801533527c8e5370c4c
Joy Latten <jlatten@cisco.com>
2023-07-26 18:48:18 -0500

    Generate the two TPM EA policies separately.
    Generate the TPM policies separately. It will then be
    easier to generate a single policy for revocation.
    
    Signed-off-by: Joy Latten <jlatten@cisco.com>

--- pkg/trust/tpmpolicy.go
+++ pkg/trust/tpmpolicy.go
@@ -1,38 +1,40 @@
 package trust
 
 import (
-	"os"
 	"errors"
 
 	"github.com/canonical/go-tpm2"
 	"github.com/canonical/go-tpm2/util"
 )
 
-type PolicyData struct {
-	Pcr7Prod       string
-	Pcr7Tpm        string
-	LuksOutFile    string
-	PasswdOutFile  string
-	PolicyVersion  string
-}
-
-// genLuksPolicy creates a policy for reading the LUKS password
-// while booted under a production key.
-func genLuksPolicy(pData PolicyData) error {
-	// Read the uki-production pcr7 value.
-	luksPcr7, err := os.ReadFile(pData.Pcr7Prod)
-	if err != nil {
-		return err
+// GenLuksPolicy creates a tpm ea policy digest using the pcr7 value
+// while booting with uki-production key. This policy is used to
+// access the luks secret in the TPM.
+// It returns the TPM EA Policy Digest that is generated.
+func GenLuksPolicy(prodPcr7 []byte, policyVersion string) ([]byte, error) {
+	// Policy Version must be 4 digits. If not given, then use Default.
+	if policyVersion == "" {
+		policyVersion = PolicyVersion.String()
+	} else {
+		if len(policyVersion) != 4 {
+			return nil, errors.New("Policy version should be a four digit string. i.e. 0001")
+		}
+		for _, c := range policyVersion {
+			if c < '0' || c > '9' {
+				return nil, errors.New("Policy version should be four digits. i.e. 0001")
+			}
+		}
 	}
+
 	// Put the pcr7 value in a tpm2.PCRSValues structure so we can compute its digest.
 	values := make(tpm2.PCRValues)
-	err = values.SetValue(tpm2.HashAlgorithmSHA256, 7, luksPcr7)
+	err := values.SetValue(tpm2.HashAlgorithmSHA256, 7, prodPcr7)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	pcrDigest, err := util.ComputePCRDigest(tpm2.HashAlgorithmSHA256, tpm2.PCRSelectionList{{Hash: tpm2.HashAlgorithmSHA256, Select: []int{7}}}, values)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	// Create a tpm2.NVPublic structure that resembles what we would have
@@ -42,68 +44,28 @@ func genLuksPolicy(pData PolicyData) error {
 
 	trial := util.ComputeAuthPolicy(tpm2.HashAlgorithmSHA256)
 	trial.PolicyPCR(pcrDigest, tpm2.PCRSelectionList{{Hash: tpm2.HashAlgorithmSHA256, Select: []int{7}}})
-	trial.PolicyNV(nvpub.Name(), []byte(pData.PolicyVersion), 0, tpm2.OpEq)
-	policyDigest := trial.GetDigest()
-	return os.WriteFile(pData.LuksOutFile, policyDigest, 0400)
+	trial.PolicyNV(nvpub.Name(), []byte(policyVersion), 0, tpm2.OpEq)
+	return trial.GetDigest(), nil
 }
 
-func genPasswdPolicy(pData PolicyData) error {
-	// Read the uki-tpm pcr7 value.
-	Pcr7Pwd, err := os.ReadFile(pData.Pcr7Tpm)
-	if err != nil {
-		return err
-	}
+// GenPasswdPolicy creates a tpm ea policy digest using the pcr7 value
+// while booting with uki-tpm key. This policy is used to
+// access the tpm password in the TPM.
+// It returns the TPM EA Policy Digest that is generated.
+func GenPasswdPolicy(tpmPcr7 []byte) ([]byte, error) {
 	// Put the pcr7 value in a tpm2.PCRSValues structure so we can compute its digest.
 	values := make(tpm2.PCRValues)
-	err = values.SetValue(tpm2.HashAlgorithmSHA256, 7, Pcr7Pwd)
+	err := values.SetValue(tpm2.HashAlgorithmSHA256, 7, tpmPcr7)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	pcrDigest, err := util.ComputePCRDigest(tpm2.HashAlgorithmSHA256, tpm2.PCRSelectionList{{Hash: tpm2.HashAlgorithmSHA256, Select: []int{7}}}, values)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	// Use a "trial" session to compute the policy digest.
 	trial := util.ComputeAuthPolicy(tpm2.HashAlgorithmSHA256)
 	trial.PolicyPCR(pcrDigest, tpm2.PCRSelectionList{{Hash: tpm2.HashAlgorithmSHA256, Select: []int{7}}})
-	policyDigest := trial.GetDigest()
-	return os.WriteFile(pData.PasswdOutFile, policyDigest, 0400)
-}
-
-func TpmGenPolicy(pData PolicyData) error {
-	// Check inputs
-	if pData.Pcr7Prod == "" || pData.Pcr7Tpm == "" {
-		return errors.New("Missing pcr7 value(s).")
-	}
-
-	if pData.LuksOutFile == "" {
-		pData.LuksOutFile = "luks_policy.out"
-	}
-
-	if pData.PasswdOutFile == "" {
-		pData.PasswdOutFile = "passwd_policy.out"
-	}
-
-	// Policy Version if given must be 4 digits. Otherwise use a default of "0001".
-	if pData.PolicyVersion == "" {
-		pData.PolicyVersion =  PolicyVersion.String()
-	} else {
-		if len(pData.PolicyVersion) != 4 {
-			return errors.New("Policy version should be a four digit string. i.e. 0001")
-		}
-		for _, c := range pData.PolicyVersion {
-			if c < '0' || c > '9' {
-				return errors.New("Policy version should be a four digit string. i.e. 0001")
-			}
-		}
-	}
-
-	if err := genLuksPolicy(pData); err != nil {
-		return err
-	}
-	if err := genPasswdPolicy(pData); err != nil {
-		return err
-	}
-	return nil
+	return trial.GetDigest(), nil
 }
